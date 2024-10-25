6b7126867afc706d7c92f7efa52d527434f19391
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-10-25 09:51:46 -0300

    Exercicio1_Aula11_LPR
    Exercicio1_Aula11_LPR feito no Visual Studio

---
+++ Aula11/Exercicio1_Aula11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1_Aula11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Digite a altura do retângulo: ");
+            retangulo.altura = double.Parse(Console.ReadLine());
+     
+            Console.WriteLine("Digite a largura do retângulo: ");
+            retangulo.largura = double.Parse(Console.ReadLine());
+
+            double area = retangulo.calcArea();
+            double perimetro = retangulo.calcPerimetro();
+
+            Console.WriteLine($"Área do retângulo: {area.ToString("F2")}");
+            Console.WriteLine($"Perímetro do retângulo: {perimetro.ToString("F2")}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula11/Exercicio1_Aula11/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1_Aula11
+{
+    public class Retangulo
+    {
+        public double altura;
+        public double largura;
+
+        public double calcArea()
+        {
+            double area = altura * largura;
+            return area;
+        }
+
+        public double calcPerimetro()
+        {
+            double perimetro = (2 * altura) + (2 * largura);
+            return perimetro;
+        }
+    }
+}
