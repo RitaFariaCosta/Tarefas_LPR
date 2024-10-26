8bebc68e7394af7912f8a70812f1e4f258162331
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-10-25 21:46:08 -0300

    Exercicio2_Aula11
    Exercicio2_Aula11

---
+++ Aula11/Exercicio2_Aula11/Funcionario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2_Aula11
+{
+    public class Funcionario
+    {
+        public string nome;
+        public double salariobruto;
+        public double porcentagem;
+        public double imposto;
+        public double salarioliquido;
+
+        public double calcularImposto()
+        {
+            if (salariobruto <= 2000)
+            {
+                imposto = ((10.0 / 100) * salariobruto);
+            }
+            else if (salariobruto <= 3000)
+            {
+                imposto = ((15.0 / 100) * salariobruto);
+            }
+            else if (salariobruto > 3000)
+            {
+                imposto = ((20.0 / 100) * salariobruto);
+            }
+            return imposto;
+        }
+
+        public double aumentarSalario()
+        {
+            salariobruto = salariobruto + (salariobruto * (porcentagem / 100));
+            return salariobruto;
+        }
+
+        public double calcularSalarioLiquido(double imposto, double salariobruto)
+        {
+            double salarioliquido = salariobruto - imposto;
+            return salarioliquido;
+        }
+
+        public void mostrarDados(double salarioliquido)
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Salário líquido: {salarioliquido}");
+        }
+    }
+}
---
+++ Aula11/Exercicio2_Aula11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2_Aula11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Digite o nome do fúncionário: ");
+            funcionario.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salário bruto do fúncionário: ");
+            funcionario.salariobruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite uma porcentagem para aumentar o salário do fúncionário: ");
+            funcionario.porcentagem = double.Parse(Console.ReadLine());
+
+            double imposto = funcionario.calcularImposto();
+            Console.WriteLine(imposto);
+            double salariobruto = funcionario.aumentarSalario();
+            Console.WriteLine(salariobruto);
+            double salarioliquido = funcionario.calcularSalarioLiquido(imposto, salariobruto);
+            funcionario.mostrarDados(salarioliquido);
+
+            //Console.ReadLine();
+
+        }
+    }
+}
