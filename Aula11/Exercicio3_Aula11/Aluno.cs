2035e789e29dee64e55833094d9243b372e22e52
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-10-25 22:57:01 -0300

    Exercicio3_Aula11
    Exercicio3_Aula11

---
+++ Aula11/Exercicio3_Aula11/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3_Aula11
+{
+    class Aluno
+    {
+        public int matricula;
+        public string nome;
+        public double notaprova1;
+        public double notaprova2;
+        public double notatrabalho;
+
+        public double media()
+        {
+            double media = (((notaprova1 * 2.5) + (notaprova2 * 2.5) + (notatrabalho * 2.0))/7);
+            return media;
+        }
+        public double final()
+        {
+            double mediaatual = media();
+            
+            if (mediaatual >= 7)
+            {
+                return 0;
+            }
+            else
+            {
+                double notanecessaria = 7 - mediaatual;
+                return notanecessaria;
+            }
+        }
+    }
+}
---
+++ Aula11/Exercicio3_Aula11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3_Aula11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno =  new Aluno();
+
+            Console.WriteLine("Digite a matrícula do aluno: ");
+            aluno.matricula = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do aluno: ");
+            aluno.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota de prova : ");
+            aluno.notaprova1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota de prova: ");
+            aluno.notaprova2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do trabalho: ");
+            aluno.notatrabalho = int.Parse(Console.ReadLine());
+
+            double media = aluno.media();
+            double final = aluno.final();
+
+            Console.WriteLine($"A média final do aluno foi de: {media.ToString("F2")} e o aluno precisa para a prova final: {final.ToString("F2")} pontos");
+
+            Console.ReadLine();
+        }
+    }
+}
