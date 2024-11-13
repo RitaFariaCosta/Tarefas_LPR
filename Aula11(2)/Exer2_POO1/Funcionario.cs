9264b2f97d2f27065c48256d4ae6512f164560dd
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-11-13 19:17:07 -0300

    Exer2_aula11_POO1
    Refazendo exercício 2 da aula 11 sobre POO

---
+++ Aula11(2)/Exer2_POO1/Funcionario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer2_POO1
+{
+    internal class Funcionario
+    {
+        public string nome { get; set; }
+        public double salarioBruto { get; set; }
+        public double salarioLiquido { get; set; }
+        public double porcentagem { get; set; }
+
+        public Funcionario(string nome, double salarioBruto, double porcentagem)
+        {
+            this.nome = nome;
+            this.salarioBruto = salarioBruto;
+            this.porcentagem = porcentagem;
+        }
+        public void mostrarDados()
+        {
+            Console.WriteLine($"Dados do funcionário {nome}: ");
+            Console.WriteLine($"Salário Líquido: {salarioLiquido}");
+
+        }
+        public double aumentarSalario()
+        {
+            if (salarioBruto <= 2000)
+            {
+                return salarioLiquido = salarioBruto + ((porcentagem / 100) * salarioBruto) - (0.10 * salarioBruto) ;
+
+            }
+            else if(salarioBruto <= 3000)
+            {
+                return salarioLiquido = salarioBruto + ((porcentagem / 100) * salarioBruto) - (0.15 * salarioBruto);
+            }
+            else
+            {
+                return salarioLiquido = salarioBruto  + ((porcentagem / 100) * salarioBruto) - (0.20 * salarioBruto);
+            }
+        }
+
+    }
+}
---
+++ Aula11(2)/Exer2_POO1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer2_POO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome, o salário bruto do funcionário e a porcentagem para aumentar o salário");
+            Funcionario funcionario = new Funcionario(Console.ReadLine(), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            funcionario.aumentarSalario();
+            funcionario.mostrarDados();
+
+            Console.ReadLine();
+
+        }
+    }
+}
