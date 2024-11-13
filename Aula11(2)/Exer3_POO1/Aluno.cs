31fd70bd35e301a70fcccc744255e15ab0b3ac18
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-11-13 19:54:08 -0300

    Exer3_aula11_POO1
    Exer3_aula11_POO1 (refazendo)

---
+++ Aula11(2)/Exer3_POO1/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer3_POO1
+{
+    internal class Aluno
+    {
+        public int matricula { get; }
+        public string nome { get; set; }
+        public double prova1 { get; set; }
+        public double prova2 { get; set; }
+        public double trabalho { get; set; }
+        public double media { get; set; }
+
+        public Aluno(string nome, double prova1, double prova2, double trabalho)
+        {
+            this.nome = nome;
+            this.prova1 = prova1;
+            this.prova2 = prova2;
+            this.trabalho = trabalho;
+        }
+        public double calcularMedia()
+        {
+            media = ((prova1 * 2.5) + (prova2 * 2.5) + (trabalho * 2.0))/7;
+            return media;
+                
+        }
+        public double final()
+        {
+            if (media >=7)
+            {
+                return 0;
+            }
+            else
+            {
+                return 7 - media;
+            }
+        }
+    }
+
+}
---
+++ Aula11(2)/Exer3_POO1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer3_POO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno("Rita", 7.5, 9.0, 9.5);
+            Aluno a2 = new Aluno("Gabi", 8.0, 6.0, 10.0);
+
+            double media1 = a1.calcularMedia();
+            double final1 = a1.final();
+
+            double media2 = a2.calcularMedia();
+            double final2 = a2.final();
+
+            Console.WriteLine($"A média do(a) estudante {a1.nome} é {media1.ToString("F2")}");
+            Console.WriteLine($"O(a) estudante {a1.nome} precisa de {final1.ToString("F2")} pontos para passar");
+
+            Console.WriteLine($"\nA média do(a) estudante {a2.nome} é {media2.ToString("F2")}");
+            Console.WriteLine($"O(a) estudante {a2.nome} precisa de {final2.ToString("F2")} pontos para passar");
+
+            Console.ReadLine();
+        }
+    }
+}
