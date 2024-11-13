3d575e8248d31bcb3b536d36fd51af64d804e9e5
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-11-13 18:25:04 -0300

    Exercicio1_Aula11_POO
    Refazendo os exercícios da aula 11 sobre POO

---
+++ Aula11(2)/Exer1_POO1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer1_POO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a altura e a largura do retângulo: ");
+            Retangulo retangulo = new Retangulo(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+
+            double area = retangulo.calArea();
+            double perimetro = retangulo.calcPerimetro();
+
+            Console.WriteLine($"A área do retângulo é {area} cm");
+            Console.WriteLine($"O perímetro do retângulo é {perimetro} cm");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula11(2)/Exer1_POO1/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer1_POO1
+{
+    internal class Retangulo
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+
+        public Retangulo(double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
+        }
+
+        public double calArea()
+        {
+            return Altura * Largura;
+        }
+
+        public double calcPerimetro()
+        {
+            return (Altura + Largura) * 2;
+        }
+    }
+}
